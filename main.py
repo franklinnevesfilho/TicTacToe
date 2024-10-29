afb7b9f9d36605b93399da3a616f6190073f8710
frank <franklin.neves.filho@gmail.com>
2024-10-29 15:13:29 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import game_window as window
+
+if __name__ == "__main__":
+    window.start()  # Create an instance of the Window class
+    # The Tkinter main loop is started in the Window class constructor
+    # so we don't need to do anything else here.  # The program will run until the window is closed.
